99a133b7741076b3320a27963f4add12fa2cc290
mzsjlwl <2855240636@qq.com>
2016-10-11 00:01:22 +0800

    BlogForAndroidHero

---
+++ app/src/main/java/com/handsome/qunyingzhuang/chapter_6/Anli09/SimpleDrawActivity.java
@@ -0,0 +1,15 @@
+package com.handsome.qunyingzhuang.chapter_6.Anli09;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.handsome.qunyingzhuang.R;
+
+public class SimpleDrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_draw);
+    }
+}
